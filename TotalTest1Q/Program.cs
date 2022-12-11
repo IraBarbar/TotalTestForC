d0147502c5549c847b54c4a9a201d92ccbcee00f
IraBarbar <bro998877@yahoo.com>
2022-12-11 17:28:44 +0300

    Array input at start

--- TotalTest1Q/Program.cs
+++ TotalTest1Q/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+string[] s = { "qraw", "dop2", "ji85", "5754" };
+void FindLessThree(string[] text, int a)
+{
+    bool t = false;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i].Length <= 3)
+        {
+            Console.Write(text[i] + " ");
+            t = true;
+        } 
+    }
+if (t == false)
+Console.WriteLine("There is no element less than three characters in this array!");
+}
+ShowArray(s);
+FindLessThree(s, 3);
