aa14e1bc0bb9cec8ef2a8829ac2585a4fe55e5b9
IraBarbar <bro998877@yahoo.com>
2022-12-12 09:56:54 +0300

    Input from User

--- TotalTest1Q/Program.cs
+++ TotalTest1Q/Program.cs
@@ -5,20 +5,38 @@
     Console.WriteLine();
 }
 
-string[] s = { "qraw", "dop2", "ji85", "5754" };
+
+//string[] s = { "qraw", "dop", "ji85", "5754" };
+string[] MadeNewArr()
+{
+    int size = 4;
+    string[] userArray = new string[size];
+
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        Console.WriteLine($"Введите любые символы {size - i} раза через 'ENTER' ");
+        userArray[i] = Console.ReadLine();
+    }
+    return userArray;
+}
 void FindLessThree(string[] text, int a)
 {
     bool t = false;
     for (int i = 0; i < text.Length; i++)
     {
-        if (text[i].Length <= 3)
+        if (text[i].Length <= a)
         {
             Console.Write(text[i] + " ");
             t = true;
-        } 
+        }
     }
-if (t == false)
-Console.WriteLine("There is no element less than three characters in this array!");
+    if (t == false)
+        Console.WriteLine("There is no element less than three characters in this array!");
+    else
+        Console.Write($" - в этих значениях длина, где  <= {a} символа !");
 }
-ShowArray(s);
-FindLessThree(s, 3);
+string[] yorArray = MadeNewArr();
+Console.Write($" Ваш массив: ");
+ShowArray(yorArray);
+
+FindLessThree(yorArray, 3);
